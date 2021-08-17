cac98925f4e1db8366cb8ab5ac147152f1157a8a
Bryan <bryantyz98@gmail.com>
2021-08-17 14:50:26 +0800

    js

---
+++ scripts/posts.js
@@ -0,0 +1,22 @@
+var postList = [
+    {
+        name: "Bob",
+        grade: "Primary",
+        subject: [],
+        price: 2,
+        location: "Woodlands"
+    }
+]
+
+
+var name = document.getElementById("Name")
+var grade = document.getElementById("column_select")
+var subject = document.getElementById("layout_select")
+var price = document.getElementById("Price_Range")
+var location = document.getElementById("Location")
+
+
+// document.getElementById("submit").addEventListener("click", handler9);
+// function handler9() {
+  
+// }
