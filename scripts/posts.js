d0f128f1ceaaff33c0a4242c60a1915077065cff
Bryan <bryantyz98@gmail.com>
2021-08-18 13:22:34 +0800

    Update posts.js

--- scripts/posts.js
+++ scripts/posts.js
@@ -9,11 +9,11 @@ var postList = [
 ]
 
 
-var name = document.getElementById("floatingInput")
+var name = document.getElementById("Username")
 var grade = document.getElementById("column_select")
 var subject = document.getElementById("layout_select")
 var price = document.getElementById("Price_Range")
-// var location = document.getElementById("Location")
+// var location = document.getElementById("studentlocation")
 
 
 // document.getElementById("submit").addEventListener("click", handler9);
