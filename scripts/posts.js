de783793c305def8ca823db883eaad736c14e614
Bryan <bryantyz98@gmail.com>
2021-08-18 13:17:38 +0800

    js

--- scripts/posts.js
+++ scripts/posts.js
@@ -9,11 +9,11 @@ var postList = [
 ]
 
 
-var name = document.getElementById("Name")
+var name = document.getElementById("floatingInput")
 var grade = document.getElementById("column_select")
 var subject = document.getElementById("layout_select")
 var price = document.getElementById("Price_Range")
-var location = document.getElementById("Location")
+// var location = document.getElementById("Location")
 
 
 // document.getElementById("submit").addEventListener("click", handler9);
