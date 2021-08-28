607a491bc9294aad15c5ce869ee5c8ae41eec804
baldr29 <baldrch@outlook.com>
2021-08-28 22:21:27 +0800

    added support for sqlite, database skeleton, will be updated every 2 hours

---
+++ scripts/auth.js
@@ -0,0 +1,10 @@
+// this script handles basic user authentication (login and register)
+
+// student accounts
+function getName() {
+    var sName = document.getElementById('sfloatingInput').value;
+    console.log(sName) //test
+};
+
+
+// tutor accounts
