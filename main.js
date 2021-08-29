e02c86d2c86e499c6f6ae1bd5b42301064266f61
Balwinder <baldrch@outlook.com>
2021-08-29 14:38:28 +0800

    Removed files, adjusted packages
    Removed sqlite3 and related files, will replace with JSON by tmr.
    
    Removed forms.html (someone added an old file yestdy): To prevent old files from being readded, please delete your local repository/files, and redo the git clone.
    
    Adjusted electron to support developer tools for in app debugging. You should be using electron instead of live-server from now on.
    
    Renamed map notes txt to notes.txt

--- main.js
+++ main.js
@@ -6,24 +6,9 @@ function createWindow() {
         width: 800, //temp
         height: 600 //temp
     })
-
+    win.webContents.openDevTools() // enable dev tools within electron.
     win.loadFile('views/index.html')
 }
-
-// initialise sqlite
-const sqlite3 = require('sqlite3').verbose();
-// check if connection to database is possible and allow read/write access
-const db = new sqlite3.Database('database/profiles.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to database.');
-})
-// create databse table
-db.serialize(function() {
-    db.run("CREATE TABLE if not exists users ( uname VARCHAR(255), pass VARCHAR(255), type VARCHAR(255), name VARCHAR(255), grade VARCHAR(255), subject VARCHAR(255), price VARCHAR(255), location VARCHAR(255));");
-});
-
 
 app.whenReady().then(() => {
     createWindow()
