88f7fafe16f319afe24fdf4a91b9fb795f51368e
Balwinder <baldrch@outlook.com>
2021-09-01 18:48:43 +0800

    Failed to add any form of database emulation
    Due to a lack of session management/database server api, persistent logins cannot occur deeming the js database emulation code useless. I have removed some of the db code for clarity and kept some as comments to serve as evidence of the emul. database's existence.

---
+++ database/users.js
@@ -0,0 +1,29 @@
+/*
+// this script serves as a temporary database. It is inefficient, and bad practice, but does its job for the prototype.
+
+// Add precreated users here
+
+let defaultUsers = [
+    {
+        "uname": "student",
+        "pass": "student",
+        "name": "Student1",
+        "grade": "Primary 4",
+        "subject": "Math",
+        "price": "$40 - $50",
+        "location": "Woodlands"
+    },
+    {
+        "uname": "tutor",
+        "pass": "tutor",
+        "name": "Tutor1",
+        "grade": "Primary 4",
+        "subject": "Math",
+        "price": "$40 - $50",
+        "location": "Orchard"
+    }
+];
+
+var sUsers = [];
+
+var tUsers = [];*/
--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,46 @@
-// this script handles basic user authentication (login and register)
+/*
+// this script handles basic user authentication without the use of a database api (login and register)
 
-// student accounts
+var sUser;
 
+// student tab login
+function sloginCheck() {
+    var sUname = document.getElementById("sfloatingInput").value;
+    var sPass = document.getElementById("sfloatingPassword").value;
+    console.log(sUname, sPass);
+    // check if typed values exist in array of default users
+    for (var i = 0; i < defaultUsers.length; i++) {
+        if (sUname == defaultUsers[i].uname && sPass == defaultUsers[i].pass) {
+            alert("Welcome " + defaultUsers[i].name);
+            document.getElementById("slogin").setAttribute("href", "sHome.html");
+            break; 
+            // if uname doesnt exist in database, alert.
+        }
+    }
+};
 
-// tutor accounts
+// tutor tab login
+function tloginCheck() {
+    var tUname = document.getElementById("tfloatingInput").value;
+    var tPass = document.getElementById("tfloatingPassword").value;
+    console.log(tUname, tPass);
+    // check if typed values exist in array of default users
+    for (var i = 0; i < defaultUsers.length; i++) {
+        if (tUname == defaultUsers[i].uname && tPass == defaultUsers[i].pass) {
+            alert("Welcome " + defaultUsers[i].name);
+            document.getElementById("tlogin").setAttribute("href", "tHome.html");
+            break
+            // if uname doesnt exist in database, alert.
+
+        }
+    }
+};
+
+// cannot be done since there is no way to manage user sessions and keep track of new users without a server
+function sRegister() {
+    pass
+};
+
+function tRegister() {
+    pass
+};*/
